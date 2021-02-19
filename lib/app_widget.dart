c4cceafd7c39c2ac0de1c88345dfd1bee14c9ac0
lumilitas <lucas23121995>
2021-02-19 15:42:25 -0300

    first commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.red),
+        home: HomePage(),
+      );
+  }
+
+}
