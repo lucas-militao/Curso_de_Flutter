8a8612ad438d32dfc9382b710d617e98c5ea002f
lumilitas <lucas23121995>
2021-02-20 11:27:14 -0300

    Gerenciar outro estado - Dark Theme

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
 
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
   final String title;
-
   const AppWidget({Key key, this.title}) : super(key: key);
-  
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: HomePage(),
-      );
+      return AnimatedBuilder(
+        animation: AppController.instance, 
+        builder: (context, child) {
+          return MaterialApp(
+            theme: ThemeData(
+              brightness: AppController.instance.isDarkTheme ? Brightness.dark : Brightness.light,
+              primarySwatch: Colors.red
+            ),
+            home: HomePage(),
+          );
+        },
+    );
   }
 
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
 
 class HomePage extends StatefulWidget{
   @override
@@ -17,17 +18,12 @@ class HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Container(
-        height: 200,
-        width: 200,
-        color: Colors.black,
-        child: Center(
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.green,
-          ),
-        ) 
+      body: Center (
+        child: Switch(
+          value: AppController.instance.isDarkTheme,
+          onChanged: (value) {
+            AppController.instance.changeTheme();
+          },),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
