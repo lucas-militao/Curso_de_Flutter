b7a71b9e94c9b80d17620eccf67613364e76ab0f
lumilitas <lucas23121995>
2021-02-21 09:18:19 -0300

    Stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,64 +10,75 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  Widget _body() {
+    return Column(children: [
+      SingleChildScrollView(
+        child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset('assets/images/logo.png'),
+                  ),
+                  Container(
+                    height: 0,
+                  ),
+                  TextField(
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Email', border: OutlineInputBorder()),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextField(
+                    onChanged: (text) {
+                      password = text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: 'Password', border: OutlineInputBorder()),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      if (email == 'lucasmilitao@email.com' &&
+                          password == 'lucas2312') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('login incorreto');
+                      }
+                    },
+                    child: Text('Entrar'),
+                  )
+                ],
+              ),
+            )),
+      ),
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView (
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-                  mainAxisAlignment:MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              Container(
-                height: 0,
-              ),
-
-              TextField(
-                onChanged: (text) {
-                  email = text;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder()
-                ),
-              ),
-
-              SizedBox(height: 15,),
-              TextField(
-                onChanged: (text) {
-                  password = text;
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder()
-                ),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  if (email == 'lucasmilitao@email.com' && password == 'lucas2312') {
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  } else {
-                    print('login incorreto');
-                  }
-                },
-                child: Text('Entrar'),  
-              )
-            ],
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset('assets/images/background.jpg', fit: BoxFit.cover,)
           ),
-          )
-        ),
-      )
+          Container(color: Colors.black.withOpacity(0.3),),
+          _body(),
+        ],
+      ),
     );
   }
 }
